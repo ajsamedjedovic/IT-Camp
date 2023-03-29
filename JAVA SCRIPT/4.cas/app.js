ff817e96fdf4bd25f39a9e7fa7f27b41f88af0bd
ajsamedjedovic <medjedovic.ajsa2005@gmail.com>
2023-03-29 23:00:58 +0200

    operatori/java/zadaci

---
+++ JAVA SCRIPT/4.cas/app.js
@@ -0,0 +1,28 @@
+//  kondicionali:
+
+if ("prvi uslov") {
+  //blok koda koji ce da se izvrsava ako taj prvi uslov bude zadovoljen
+} else if ("novi uslov") {
+  //blok koda koji ce da se izvrsava ako taj drugi uslov bude zadovoljen
+} else {
+  //blok koda koji ce se izvrsiti u slucaju da sve prethodno mije  zadovoljeno
+}
+
+const broj_godina = 18;
+
+if (broj_godina >= 18) {
+  console.log("punoletni ste");
+} else {
+  console.log("maloletni ste");
+}
+
+let x = 5;
+let y = 0;
+
+if (y != 0) console.log(x / y);
+else {
+  console.log("deljenje je nemoguce.");
+}
+
+//domaci
+//
