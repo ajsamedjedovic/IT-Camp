7f5eead2aa1fe004e873f6912d2f14a8bb446142
ajsamedjedovic <medjedovic.ajsa2005@gmail.com>
2023-05-10 21:20:05 +0200

    stringovi

---
+++ JAVA SCRIPT/15.cas/app2.js
@@ -0,0 +1,22 @@
+console.log("node.js");
+
+//STRINGOVI//
+//Stringovi su imutable vrednosti.
+//Stringovi su primitivnog(osnovnog) tipa podataka
+
+//String mozemo zapisivati na nekoliko nacina:
+// 1. koristeci duple navodnike
+// 2. |-|  obicne navodnike
+// 3. Template Literals (Back-Tics sintaksa)
+// `rerejd` (iznad taba)
+
+//Dupli navodnici:
+// "Danas je bilo "promenjivo" vreme"
+//
+// ("Danas je bilo 'promenjivo' vreme");
+// ("Danas je bilo \"promenjivo\" vreme");
+
+// //obicni navodnici
+// ('Danas je bilo "promenjivo" vreme');
+
+// ('Danas je bilo \'promenjivo\' vreme');
