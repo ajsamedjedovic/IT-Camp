09847bf64381208ab4612e5c5e50900cf1a9999f
ajsamedjedovic <medjedovic.ajsa2005@gmail.com>
2023-06-12 19:10:39 +0200

    domaci

--- JAVA SCRIPT/28.cas/zadaci.js
+++ JAVA SCRIPT/28.cas/zadaci.js
@@ -14,3 +14,34 @@ console.log(funkcija1(98, 56));
 //domaci da li su dva broja u opseku 40 do 60 ili 70 i 100.true oba su u barem jedmon,false nijedan u nijednom
 
 //veci broj u opseku od 40 do 60
+
+function funkcija(prvi, drugi) {
+  let op1 = 0;
+  let op2 = 0;
+
+  for (let i = 40; i < 60; i++) {
+    if (i === prvi || i === drugi) {
+      op1++;
+    }
+  }
+  for (let i = 70; i < 100; i++) {
+    if (i === prvi || i === drugi) {
+      op2++;
+    }
+  }
+  if (op1 > 0 && op2 > 0) {
+    return true;
+  } else if (op1 == 2 || op2 == 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(funkcija(77, 55));
+console.log(funkcija(1, 55));
+console.log(funkcija(55, 75));
+console.log(funkcija(1, 75));
+console.log(funkcija(1, 5));
+console.log(funkcija(77, 89));
+console.log(funkcija(55, 44));
