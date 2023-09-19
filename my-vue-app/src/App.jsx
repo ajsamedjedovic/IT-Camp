4f8870b993c4cf4e9e7fdf53a5ab785d098b2062
ajsamedjedovic <medjedovic.ajsa2005@gmail.com>
2023-09-19 21:00:45 +0200

    my vaue app

---
+++ my-vue-app/src/App.jsx
@@ -0,0 +1,19 @@
+import "/App.css";
+
+function App() {
+  return (
+    <div style={{ display: "flex" }} class="rootdiv">
+      <p>
+        ailblocks.cc traffic volume is 1,211 unique daily visitors and their
+        1,211 pageviews. The web value rate of tailblocks.cc is 12,498 USD. Each
+        visitor makes around 1.07 page views on average. By Alexa's traffic
+        estimates tailblocks.cc placed at 7,503 position over the world.
+      </p>
+      <div>
+        <button>Click here</button>
+        <a href=""> link</a>
+      </div>
+    </div>
+  );
+}
+export default App;
